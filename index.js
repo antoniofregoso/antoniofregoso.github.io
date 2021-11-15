adc1a3a57f471dc2cbf7daeb06979a598b35c749
antoniofregoso <yomero@antoniofregoso.com>
2021-11-15 17:38:09 -0600

    [ADD]

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import RevealAnimate from './reveal.js-animate/animate.esm.js';
 let deck1 = new Reveal(document.querySelector( '.deck1' ), {
     animated: {
         'H1':{'animated':'backInDown'},
-        'H2':{'animated':'bounceIn'},
+        'H2':{'animated':'bounceIn', 'repeat':'repeat-3'},
         'P':{'animated':'fadeInUpBig'},
         'UL':{'animated':'fadeInTopLeft'},
         'OL':{'animated':'jackInTheBox'},
